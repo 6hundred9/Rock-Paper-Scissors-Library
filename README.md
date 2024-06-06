1dd21b42e186b02b3cdb2c9103fa085d0f9de947
6hundred9 <66hundred99@gmail.com>
2024-06-06 10:45:01 +0300

    Updates

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Rock-Paper-Scissors-Library
+﻿# Rock-Paper-Scissors-Library
 A Simple NuGet library for rock paper scissors
 
 ### Documentation
@@ -11,4 +11,4 @@ Definition: public static State Play(Rps player1, Rps player2)
 Arguements:
 - Player 1: The [input](https://github.com/6hundred9/Rock-Paper-Scissors-Library/blob/main/RPS.cs) of player 1
 - Player 2: The [input](https://github.com/6hundred9/Rock-Paper-Scissors-Library/blob/main/RPS.cs) of player 2
-Returns: Whether player 1 won, lost or finished in a draw
+  Returns: Whether player 1 won, lost or finished in a draw
--- obj/Debug/net8.0/RockPaperScissors.AssemblyInfo.cs
+++ obj/Debug/net8.0/RockPaperScissors.AssemblyInfo.cs
@@ -10,15 +10,15 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("RockPaperScissors")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("6hundred9")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright (c) 2024 6hundred9")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("A simple API/library built for rock paper scissors")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b11a5424fce121dd7edc8a2db18513110149ee0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0+aa8e7cbb72b77d3f11267f32b640022e37076a44")]
 [assembly: System.Reflection.AssemblyProductAttribute("RockPaperScissors")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RockPaperScissors")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
 [assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/6hundred9/Rock-Paper-Scissors-Library/")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
